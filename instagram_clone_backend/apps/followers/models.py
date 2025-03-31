4563c90c67d34621f80d002add126ad5b56db7bc
musfiqamirov888 <musfiq.emirov5@gmail.com>
2025-03-31 23:29:10 +0400

    users and followers mmodels added

---
+++ instagram_clone_backend/apps/followers/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+from users.models import *
+
+
+class Follow(models.Model):
+    follower = models.ForeignKey(CustomUser, related_name='following', on_delete=models.CASCADE)
+    following = models.ForeignKey(CustomUser, related_name='followers', on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        unique_together = ('follower', 'following')
